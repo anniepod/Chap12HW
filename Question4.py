ac0e1af4af471965482922629d2faa4eb6c51ac5
anniepod <31967599+anniepod@users.noreply.github.com>
2018-04-25 22:32:35 -0400

    new

---
+++ Question4.py
@@ -0,0 +1,11 @@
+import mymodule1
+
+import mymodule2
+
+print( (mymodule2.myage - mymodule1.myage) ==
+       (mymodule2.year - mymodule1.year)  )
+
+print("My name is", __name__)
+
+if __name__ == "__main__":
+    print("This won't run if I'm  imported.")
